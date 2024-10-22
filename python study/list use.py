cb5b95ab5a95db60cde535c574d0b708ac9952cb
Vaseti <3044292419@qq.com>
2024-10-22 15:37:24 +0800

    list use

---
+++ python study/list use.py
@@ -0,0 +1,16 @@
+classmates=['Mike','Xiaoming','Xiaohong','Jock','Jonh']
+#正序输出
+n=0
+while n<len(classmates):
+    print('Hello,'+classmates[n])
+    n+=1
+#倒序输出
+m=len(classmates)
+while m>0:
+    m-=1
+    print('This is a good day,'+classmates[m])
+classmates.append('Whitch')
+classmates.insert(0,'Pike')
+classmates.pop(2)
+classmates[3]='Black'
+print(classmates)
